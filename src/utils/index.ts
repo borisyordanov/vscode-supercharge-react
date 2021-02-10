70793be7ff043b39d79b8c232b61d86fd796a8df
Boris Yordanov <boris.yordanov@toptal.com>
2021-02-10 09:38:00 -0400

    chore: delete unused function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
 export { default as getAllModuleNames } from "./get-all-module-names";
 export { default as getDirectories } from "./get-directories";
-export { default as getSelectedText } from "./get-selected-text";
 export * from "./davinci";
